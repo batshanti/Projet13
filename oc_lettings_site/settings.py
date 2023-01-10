9dfe9ee9ae8bfcc5b5ce148e390c5edd9d266a20
Batshanti <teddylouvet@yahoo.fr>
2023-01-10 11:23:51 +0100

    add home, profiles app

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'oc_lettings_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../lettings/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
