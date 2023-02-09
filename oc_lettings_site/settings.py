a0e33bedb9438bda42312dda7a48921895149c52
Batshanti <teddylouvet@yahoo.fr>
2023-02-09 22:43:14 +0100

    add localhost

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'oc-lettingss.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'oc-lettingss.herokuapp.com']
 
 
 # Application definition
