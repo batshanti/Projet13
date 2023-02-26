13cb767df2297b5603cb9fe641e59f096cb35063
Louvet Teddy <teddylouvet@yahoo.fr>
2023-02-26 22:02:52 +0100

    Update urls.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
 
 urlpatterns = [
     path('', include('home.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
-    path('admin/', admin.site.urls)
-
+    path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error)
 ]
