f5b5ec947a9e9da6c83022d81e7735d7924faea1
Louvet Teddy <teddylouvet@yahoo.fr>
2023-02-26 22:06:18 +0100

    Update urls.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,6 +6,7 @@ def trigger_error(request):
     division_by_zero = 1 / 0
     return division_by_zero
 
+
 urlpatterns = [
     path('', include('home.urls')),
     path('lettings/', include('lettings.urls')),
